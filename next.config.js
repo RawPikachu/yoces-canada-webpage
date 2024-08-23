9da1c19bb34eacb9f720cb505c88fbc46d778ce4
Raw <>
2024-08-22 20:55:04 -0400

    move hackathon page to subdomain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'mdx']
+    pageExtensions: ['js', 'jsx', 'mdx'],
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: '/hackathon',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'hackathon.yoces.ca',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = withMDX(nextConfig)
