5ae90d7e30bd76a79af6d0f0720efb89cd457687
Raw <>
2024-08-22 21:44:43 -0400

    attempt to fix this subdomain thing

--- middleware.js
+++ middleware.js
@@ -4,10 +4,9 @@ export function middleware(req) {
   const hostname = req.headers.get('host');
   const subdomain = hostname.match(/^([^.]+)\./)?.[1];
 
-  switch (true) {
-    case subdomain.startsWith('hackathon'):
-      return NextResponse.rewrite(new URL(`/hackathon${req.nextUrl.pathname}`, req.url));
-    default:
-      return NextResponse.next();
+  if (subdomain && subdomain.startsWith('hackathon')) {
+    return NextResponse.rewrite(new URL('/hackathon', req.url));
   }
+
+  return NextResponse.next();
 }
