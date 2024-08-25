68ff1a38e03f794e75e37dc837fc939b09117b14
RawPikachu <56405082+RawPikachu@users.noreply.github.com>
2024-08-25 01:00:22 -0400

    Temporary change

--- components/navbar.js
+++ components/navbar.js
@@ -27,7 +27,7 @@ const Navbar = (props) => {
             sx={{ height: 48, textAlign: 'left' }}
           />
           <Box />
-          <NavLink href="https://hackathon.yoces.ca">
+          <NavLink href="https://www.yoces.ca">
             Hackathon
           </NavLink>
           <NavLink href="mailto:youthoces@gmail.com">
